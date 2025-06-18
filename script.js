779d3c1c2462e1bfe4d42684fe0ad8703c2abfb8
Adnan Malik <adnan.malik@Optimum.com>
2025-06-18 11:32:59 -0400

    Guest

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-// Wait for form to be submitted
+// unhide guest when guest is selected
+
+document.getElementById("statusSelect").addEventListener("change", function () {
+    const selected = this.value;
+    const guestField = document.getElementById("guestField");
+
+    const guest1fname = document.querySelector('input[name="guest1fname"]');
+    const guest1lname = document.querySelector('input[name="guest1lname"]');
+    const guest2fname = document.querySelector('input[name="guest2fname"]');
+    const guest2lname = document.querySelector('input[name="guest2lname"]');
+
+    if (selected == "guest") {
+      guestField.classList.remove("hidden");
+      guest1fname.required = true;
+      guest1lname.required = true;
+      guest2fname.required = true;
+      guest2lname.required = true;
+    }
+    else {
+      guestField.classList.add("hidden");
+      guest1fname.required = false;
+      guest1lname.required = false;
+      guest2fname.required = false;
+      guest2lname.required = false;
+    }
+
+  });
+
+
+  // Wait for form to be submitted
 
 document.getElementById("rsvpForm").addEventListener("submit", async function (e) {
     e.preventDefault(); // prevents page from reloading when form is submitted
@@ -14,6 +43,7 @@ document.getElementById("rsvpForm").addEventListener("submit", async function (e
     status: form.status.value,     // Value from the dropdown (student/guest)
     dietary: form.dietary.value    // Value from the dietary input
   };
+
 
   console.log("Form submitted:", data); // Print the data to the browser console for testing
 
